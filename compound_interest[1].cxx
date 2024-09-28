080c4cb59e9ebc500c08c4d738613d2975e478e2
Kelvin269-coder <kelvinwaithera766@gmail.com>
2024-09-28 05:37:48 -0700

    Add files via upload

---
+++ compound_interest[1].cxx
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+double p,r,t,ci,a;
+printf("please enter principle,rate,time =",p,r,t);
+scanf("%lf,%lf,%lf",&p,&r,&t);
+
+a=p*pow((1+r/100),t);
+ci=a-p;
+printf ("amount is %.2lf",a);
+printf("the compound interest is %.2lf:",ci);
+
+return 0;
+}
